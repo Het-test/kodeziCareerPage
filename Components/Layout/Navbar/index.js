9387271c70a47ee92398360fce1a1a72d027f2bd
hetal <>
2022-09-09 19:32:49 +0530

    Kodezi header responsive using hamburger menu icon

--- Components/Layout/Navbar/index.js
+++ Components/Layout/Navbar/index.js
@@ -7,10 +7,12 @@ import Typography from "@mui/material/Typography";
 import { Grid, Link as MUILink, Menu, MenuItem, Popover } from "@mui/material";
 import Link from "next/link";
 import SVG from "react-inlinesvg";
-import Button from "@mui/material/Button";
 import * as s from "./styles/navbar.style";
+import Hidden from '@mui/material/Hidden';
+import { Button } from "@mui/material";
+import IconButton from '@mui/material/IconButton';
 
-import { logo, profile_icon } from "../../../assets/svgs/pageSvgs";
+import { logo, profile_icon, humburgMenuIcon } from "../../../assets/svgs/pageSvgs";
 import Item from "../../Item";
 
 const navItems = ["Product", "Integrations", "Code Privacy", "Pricing"];
@@ -45,134 +47,150 @@ const Navbar = () => {
               <SVG src={logo} />
             </a>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              paddingLeft: "50px",
-              alignItems: "center",
-              flexGrow: "1",
-            }}
-          >
-            <Box className="header-link">
-              <Button disableRipple onClick={handleClickProduct}>
-                Product
-              </Button>
-              <Popover
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
+          <Hidden mdDown={true} xlDown={false}>
+            <Box
+              sx={{
+                display: "flex",
+                paddingLeft: "50px",
+                alignItems: "center",
+                flexGrow: "1",
+              }}
+            >
+              <Box className="header-link">
+                <Button disableRipple onClick={handleClickProduct}>
+                  Product
+                </Button>
+                <Popover
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                >
+                  <Box component="div">
+                    <Grid>
+                      <Item>
+                        <Box>
+                          <span>#icon</span>
+                          <span>Kodezi Code</span>
+                        </Box>
+                        <Typography>
+                          Try our live web-based IDE with features built in!
+                        </Typography>
+                      </Item>
+                      <Item>
+                        <Box>
+                          <span>#icon</span>
+                          <span>Kodezi cloud</span>
+                        </Box>
+                        <Typography>
+                          Auto-correct your codebase within VS Code and optimize
+                          your codebase!
+                        </Typography>
+                      </Item>
+                    </Grid>
+                  </Box>
+                </Popover>
+              </Box>
+              <Box className="header-link">
+                <Button disableRipple onClick={handleClickIntegration}>
+                  Integrations
+                </Button>
+                <Popover
+                  open={openIntegration}
+                  anchorEl={anchorEl2}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                >
+                  <Box>
+                    <Grid>
+                      <Item>
+                        <Box>
+                          <span>#icon</span>
+                          <span>Web App IDE Beta</span>
+                        </Box>
+                        <Typography>
+                          Try our live web-based IDE with features built in!
+                        </Typography>
+                      </Item>
+                      <Item>
+                        <Box>
+                          <span>#icon</span>
+                          <span>VS Code</span>
+                        </Box>
+                        <Typography>
+                          Auto-correct your codebase within VS Code and optimize
+                          your codebase!
+                        </Typography>
+                      </Item>
+                      <Item>
+                        <Box>
+                          <span>#icon</span>
+                          <span>GitHub Soon</span>
+                        </Box>
+                        <Typography>
+                          Auto-correct your codebase within VS Code and optimize
+                          your codebase!
+                        </Typography>
+                      </Item>
+                      <Item>
+                        <Box>
+                          <span>#icon</span>
+                          <span>Slack soon</span>
+                        </Box>
+                        <Typography>
+                          Auto-correct your codebase within VS Code and optimize
+                          your codebase!
+                        </Typography>
+                      </Item>
+                    </Grid>
+                  </Box>
+                </Popover>
+              </Box>
+              <Box className="header-link">
+                <Link href="#">Code Privacy</Link>
+              </Box>
+              <Box className="header-link">
+                <Link href="#">Pricing</Link>
+              </Box>
+            </Box>
+            <Box
+              className="right-header"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Typography className="text-bg">Kodezi vs Copilot</Typography>
+              <Box className="down-box">
+                <Button className="download-btn">Download</Button>
+              </Box>
+              <Box sx={{ paddingLeft: "15px", position: "relative", top: "2px" }}>
+                <Link href="#">
+                  <SVG src={profile_icon} />
+                </Link>
+              </Box>
+            </Box>
+          </Hidden>
+          <Hidden mdUp={true}>
+            <div style={{ width: '100%' }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
                 }}
               >
-                <Box component="div">
-                  <Grid>
-                    <Item>
-                      <Box>
-                        <span>#icon</span>
-                        <span>Kodezi Code</span>
-                      </Box>
-                      <Typography>
-                        Try our live web-based IDE with features built in!
-                      </Typography>
-                    </Item>
-                    <Item>
-                      <Box>
-                        <span>#icon</span>
-                        <span>Kodezi cloud</span>
-                      </Box>
-                      <Typography>
-                        Auto-correct your codebase within VS Code and optimize
-                        your codebase!
-                      </Typography>
-                    </Item>
-                  </Grid>
-                </Box>
-              </Popover>
-            </Box>
-            <Box className="header-link">
-              <Button disableRipple onClick={handleClickIntegration}>
-                Integrations
-              </Button>
-              <Popover
-                open={openIntegration}
-                anchorEl={anchorEl2}
-                onClose={handleClose}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-              >
-                <Box>
-                  <Grid>
-                    <Item>
-                      <Box>
-                        <span>#icon</span>
-                        <span>Web App IDE Beta</span>
-                      </Box>
-                      <Typography>
-                        Try our live web-based IDE with features built in!
-                      </Typography>
-                    </Item>
-                    <Item>
-                      <Box>
-                        <span>#icon</span>
-                        <span>VS Code</span>
-                      </Box>
-                      <Typography>
-                        Auto-correct your codebase within VS Code and optimize
-                        your codebase!
-                      </Typography>
-                    </Item>
-                    <Item>
-                      <Box>
-                        <span>#icon</span>
-                        <span>GitHub Soon</span>
-                      </Box>
-                      <Typography>
-                        Auto-correct your codebase within VS Code and optimize
-                        your codebase!
-                      </Typography>
-                    </Item>
-                    <Item>
-                      <Box>
-                        <span>#icon</span>
-                        <span>Slack soon</span>
-                      </Box>
-                      <Typography>
-                        Auto-correct your codebase within VS Code and optimize
-                        your codebase!
-                      </Typography>
-                    </Item>
-                  </Grid>
-                </Box>
-              </Popover>
-            </Box>
-            <Box className="header-link">
-              <Link href="#">Code Privacy</Link>
-            </Box>
-            <Box className="header-link">
-              <Link href="#">Pricing</Link>
-            </Box>
-          </Box>
-          <Box
-            className="right-header"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <Typography className="text-bg">Kodezi vs Copilot</Typography>
-            <Box className="down-box">
-              <Button className="download-btn">Download</Button>
-            </Box>
-            <Box sx={{ paddingLeft: "15px", position: "relative", top: "2px" }}>
-              <Link href="#">
-                <SVG src={profile_icon} />
-              </Link>
-            </Box>
-          </Box>
+                <Button aria-label="menu" sx={{ paddingRight: '20px', justifyContent: 'flex-end', height: '35px', width: '35px' }}>
+                  <SVG src={humburgMenuIcon} />
+                </Button>
+              </Box>
+            </div>
+          </Hidden>
         </Box>
       </Container>
     </s.Header>
--- assets/svgs/pageSvgs.js
+++ assets/svgs/pageSvgs.js
@@ -186,3 +186,10 @@ export const inclusivitySvgIcon = `<svg width="72" height="72" viewBox="0 0 72 7
 </defs>
 </svg>
 `;
+
+export const humburgMenuIcon = `<svg width="22" height="14" viewBox="0 0 22 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 7H21" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 13H21" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 1H21" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
